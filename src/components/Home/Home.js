f2709c63cac246ebc73d7509af8d8aa0905f994f
Martin Galli <martin.ggalli@gmail.com>
2019-08-29 14:54:50 -0300

    Images and Home Init

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import classes from './Home.css'
 
+import JujuyBackground from '../../assets/images/jujuy.jpg'
+
+import Title from '../UI/Title/Title'
+
 class Home extends Component {
     render () {
         return (
             <div className={classes.Home}>
-                <h1>HOME</h1>
+                <section className={classes.HomeSection} style={{backgroundImage: `url(${JujuyBackground})`}}>
+                    <div className={classes.TitlePosition}>
+                        <Title titleClass={classes.Title} title="ARGENTINA SIN ATAJOS"/>
+                    </div>
+                </section>
             </div>
         )
     }
